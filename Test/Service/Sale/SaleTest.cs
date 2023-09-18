dc3d203a63a4866bf364a426eafc24579b2ed91a
PabloLoading <pabloduransc@gmail.com>
2023-09-18 14:06:58 -0300

    RED CanCreateSale_Ok

---
+++ Test/Service/Sale/SaleTest.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Test.Service.Sale;
+using Service;
+
+[TestClass]
+public class SaleTest
+{
+    [TestMethod]
+    public void CanCreateSale_Ok()
+    {
+        Sale s = new Sale();
+        Assert.IsNotNull(s);
+    }
+}
