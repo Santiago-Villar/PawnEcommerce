5f8fd2b868d2507cda8db22aceeaec1e6279adab
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-09-18 16:38:24 -0300

    RED- CreateProductOk fails

---
+++ Test/ProductTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test;
+
+[TestClass]
+public class ProductTest
+{
+    Product testProduct = new Product();
+    [TestMethod]
+    public void CreateProductOk()
+    {
+        Assert.IsNotNull(testProduct);
+    }
+}
