f2bc2126879aece3db725c30122d967298b22465
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-09-19 00:07:41 -0300

    GREEN - ProductHasNegativePrice passes

--- Service/Product/Product.cs
+++ Service/Product/Product.cs
@@ -7,12 +7,15 @@
         public string Description { get; set; }
         private int _price;
         public int Price { get => _price;
-            set {  _price = value; }
+            set {  
+               if(value< 0) throw new ServiceException("Price must be a positive integer.");
+                _price = value; 
+            }
         }
         public Product(String name, String Description, int price) {
             this.Name = name;  
             this.Description = Description;
-            this.Price=price
+            this.Price = price;
         }
     }
 }
---
+++ Service/ServiceException.cs
@@ -0,0 +1,9 @@
+﻿namespace Service{ 
+    public class ServiceException:Exception
+    {
+        public ServiceException(string message):base(message) 
+        {
+            
+        }
+    }
+}
