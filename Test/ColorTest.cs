9b66e451870859b6a3d81fb979babc59ec5f79c8
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-09-19 15:52:52 -0300

    RED - ColorIsNotNull fails

---
+++ Test/ColorTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Test;
+using Service.Product;
+
+[TestClass]
+public class ColorTest
+{
+    Color aColor = new Color()
+    {
+        Name = "Green"
+    };
+
+    [TestMethod]
+    public void ColorIsNotNull()
+    {
+        Assert.IsNotNull(aColor);
+    }
+}
