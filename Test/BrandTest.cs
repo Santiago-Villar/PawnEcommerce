b0c6684dcaa32e1945de708993b0309628ef460a
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-09-19 01:05:33 -0300

    GREEN - BrandIsNotNull passes

--- Test/BrandTest.cs
+++ Test/BrandTest.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Service.Product;
 using System.Threading.Tasks;
 
 namespace Test;
@@ -10,6 +11,7 @@ namespace Test;
 [TestClass]
 public class BrandTest
 {
+    public static Brand aBrand = new Brand("Kova");
     [TestMethod]
     public void BrandIsNotNull()
     {
