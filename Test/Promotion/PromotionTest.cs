3c1c7129b7d7479e9f5deaae1900290b322a369c
MateoGiraz <mateogiraz27@gmail.com>
2023-09-19 01:18:08 -0300

    REFACTOR ApplyDiscount_TwentyPercentDiscount_Ok

--- Test/Promotion/PromotionTest.cs
+++ Test/Promotion/PromotionTest.cs
@@ -8,6 +8,19 @@ namespace Test.Promotion;
 [TestClass]
 public class PromotionTest
 {
+    private Mock<IProduct> CreateMockProduct(int price, string category)
+    {
+        var mockCategory = new Mock<ICategory>();
+        mockCategory.Setup(cat => cat.Name).Returns(category);
+        var mockedCategory = mockCategory.Object;
+        
+        var mockProduct = new Mock<IProduct>();
+        mockProduct.Setup(product => product.Category).Returns(mockedCategory);
+        mockProduct.Setup(product => product.Price).Returns(price);
+        
+        return mockProduct;
+    }
+    
     [TestMethod]
     public void CanCreateTotalLook_Ok()
     {
@@ -40,11 +53,10 @@ public class PromotionTest
     public void ApplyDiscount_TwentyPercentDiscount_Ok()
     {
         var twentyPercentDiscount = new TwentyPercentDiscount();
+        
         const int productPrice = 10;
-
-        var mockProduct = new Mock<IProduct>();
-        mockProduct.Setup(product => product.Price).Returns(productPrice);
-        var mockedProduct = mockProduct.Object;
+        const string productCategory = "Jeans";
+        var mockedProduct = CreateMockProduct(productPrice, productCategory).Object;
 
         var products = new List<IProduct>()
         {
