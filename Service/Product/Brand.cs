acc0779db6f16aa9f0063946acc4bee2dea101db
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-09-19 01:03:58 -0300

    RED - BrandIsNotNull fails

---
+++ Service/Product/Brand.cs
@@ -0,0 +1,12 @@
+﻿namespace Service.Product
+{
+    public class Brand
+    {
+        public string Name { get; set; }
+
+        public Brand(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Test/BrandTest.cs
@@ -0,0 +1,18 @@
+﻿using Service.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test;
+
+[TestClass]
+public class BrandTest
+{
+    [TestMethod]
+    public void BrandIsNotNull()
+    {
+        Assert.IsNotNull(aBrand);
+    }
+}
