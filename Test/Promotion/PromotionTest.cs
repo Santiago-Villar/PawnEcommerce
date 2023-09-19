6c4c66ebf25398ba9adab17800f0df5fb2c7deab
MateoGiraz <mateogiraz27@gmail.com>
2023-09-19 01:13:59 -0300

    RED ApplyDiscount_ThreeForTwo_Ok

--- Test/Promotion/PromotionTest.cs
+++ Test/Promotion/PromotionTest.cs
@@ -1,6 +1,7 @@
 using Service.Promotion.ConcreteStrategies;
 using Moq;
 using Service.Product;
+using Service.Product.Category;
 
 namespace Test.Promotion;
 
@@ -53,6 +54,35 @@ public class PromotionTest
 
         var discountPrice = twentyPercentDiscount.GetDiscountPrice(products);
         const float expectedDiscountPrice = 18;
+
+        Assert.AreEqual(expectedDiscountPrice, discountPrice);
+    }
+    
+    [TestMethod]
+    public void ApplyDiscount_ThreeForTwo_Ok()
+    {
+        var threeForTwoDiscount = new ThreeForTwo();
+        const int productPrice = 10;
+
+        var mockCategory = new Mock<ICategory>();
+        mockCategory.Setup(category => category.Name).Returns("Jeans");
+        var mockedCategory = mockCategory.Object;
+        
+        var mockProduct = new Mock<IProduct>();
+        mockProduct.Setup(product => product.Category).Returns(mockedCategory);
+        mockProduct.Setup(product => product.Price).Returns(productPrice);
+        var mockedProduct = mockProduct.Object;
+
+        var products = new List<IProduct>()
+        {
+            mockedProduct,
+            mockedProduct,
+            mockedProduct,
+            mockedProduct,
+        };
+
+        var discountPrice = threeForTwoDiscount.GetDiscountPrice(products);
+        const float expectedDiscountPrice = 30;
 
         Assert.AreEqual(expectedDiscountPrice, discountPrice);
     }
