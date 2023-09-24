4d3803e254d8c1efa1d7163f7a31d5e7c1bb51e3
MateoGiraz <mateogiraz27@gmail.com>
2023-09-23 23:43:58 -0300

    GREEN Match_EqualName_Ok

--- Service/Filter/ConcreteFilter/NameFilter.cs
+++ Service/Filter/ConcreteFilter/NameFilter.cs
@@ -1,6 +1,9 @@
 namespace Service.Filter.ConcreteFilter;
-
+using Service.Product;
 public class NameFilter
 {
-    
+    public bool Match(Product product, string name)
+    {
+        return product.Name == name;
+    }
 }
