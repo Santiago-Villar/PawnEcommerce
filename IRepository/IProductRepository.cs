b534689847a56688149ec8e5aece5b63e5dbb569
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-09-25 11:33:04 -0300

    Created IRepository project

---
+++ IRepository/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service;
+using Service.Product;
+using Service.User;
+
+namespace IRepository
+{
+    public interface IProductRepository
+    {
+        Product AddProduct(Product newProduct);
+        Product GetProductByName(string productName, User owner);
+        Product UpdateProduct(Product newProductVersion, string previousVersionName);
+        Product DeleteProduct(Product product);
+        Product GetAllProducts();
+        Product[] GetProductsByUser(User user);
+        Boolean Exists(Product product);
+        void Reset();
+    }
+}
