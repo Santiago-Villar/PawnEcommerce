f42288b0fc8b1fc8fd68f6285a7f88af5eeda104
MateoGiraz <mateogiraz27@gmail.com>
2023-09-25 14:35:34 -0300

    RED CanLogInUser_WrongPassword_Throw

--- Test/Service/UserTest/UserServiceTest.cs
+++ Test/Service/UserTest/UserServiceTest.cs
@@ -52,4 +52,24 @@ public class UserServiceTest
         var userService = new UserService(mockRepository.Object);
         userService.LogIn(mockUser.Object);
     }
+    
+    [ExpectedException(typeof(RepositoryException))]
+    [TestMethod]
+    public void CanLogInUser_WrongPassword_Throw()
+    {
+        var mockUser = new Mock<IUser>();
+        mockUser.Setup(user => user.Email).Returns("TestEmail@gmail.com");
+        mockUser.Setup(user => user.PasswordHash).Returns("currentPassword");
+
+        var mockToCheckUser = new Mock<IUser>();
+        mockToCheckUser.Setup(user => user.Email).Returns("TestEmail@gmail.com");
+        mockToCheckUser.Setup(user => user.PasswordHash).Returns("differentPassword");
+        
+        var mockRepository = new Mock<IUserRepository>();
+        mockRepository.Setup(repo => repo.Exists(mockToCheckUser.Object)).Returns(true);
+        mockRepository.Setup(repo => repo.Get(mockToCheckUser.Object.Email)).Returns(mockUser.Object);
+
+        var userService = new UserService(mockRepository.Object);
+        userService.LogIn(mockToCheckUser.Object);
+    }
 }
