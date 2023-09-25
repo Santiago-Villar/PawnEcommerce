9af9bfccbd63a1edea1c8171bbe9620dd058bd27
PabloLoading <pabloduransc@gmail.com>
2023-09-25 20:43:24 -0300

    RED CanCreatePromotionsLogic_Ok

---
+++ Test/Service/Promotion/PromotionLogicTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Service.Promotion;
+
+namespace Test.Service.Promotion
+{
+    [TestClass]
+    public class PromotionLogicTest
+	{
+
+		public PromotionLogicTest()
+		{
+		}
+
+        [TestMethod]
+        public void CanCreatePromotionsLogic_Ok()
+        {
+            IPromotionLogic logic = new PromotionLogic();
+            Assert.IsNotNull(logic);
+        }
+    }
+}
+
