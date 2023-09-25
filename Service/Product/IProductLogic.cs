eed5ba74f01a675a79d91476fded31323df8d6d4
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-09-25 13:18:08 -0300

    Removed unused Libraries

---
+++ Service/Product/IProductLogic.cs
@@ -0,0 +1,17 @@
+﻿using IRepository;
+using Service.Product;
+using Service.User;
+
+namespace Logic
+{
+    public interface IProductLogic
+    {
+        public Product AddProduct(Product Product);
+        public Product DeleteProduct(Product mockProduct);
+        public Product GetProductByName(string ProductName, User owner);
+        public Product[] GetProductsByOwner(User User);
+        public Product UpdateProduct(Product mockProduct, string newName);
+        public void Reset();
+        public bool NewNameIsValid(string newName, Product Product);
+    }
+}
