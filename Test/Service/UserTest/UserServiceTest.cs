77e3d15bde8efc815c0016e7b9d62524c0870b11
MateoGiraz <mateogiraz27@gmail.com>
2023-09-25 16:26:52 -0300

    REFACTOR UserServiceTest

--- Test/Service/UserTest/UserServiceTest.cs
+++ Test/Service/UserTest/UserServiceTest.cs
@@ -8,6 +8,32 @@ namespace Test.Service.UserTest;
 [TestClass]
 public class UserServiceTest
 {
+    private const string Email = "TestEmail@gmail.com";
+    private const string Password = "currentPassword";
+    private const string DifferentPassword = "differentPassword";
+    private const string ToUpdateAddress = "1234 Laughter Lane";
+    private const string NewAddress = "101 Prankster Place";
+
+    private IUser GetMockUser()
+    {
+        var mockUser = new Mock<IUser>();
+        mockUser.Setup(user => user.Email).Returns(Email);
+        mockUser.Setup(user => user.Address).Returns(ToUpdateAddress);
+        mockUser.Setup(user => user.PasswordHash).Returns(HashPassword(Password));
+        
+        return mockUser.Object;
+    }
+    
+    private IUser GetSecondaryMockUser()
+    {
+        var mockUser = new Mock<IUser>();
+        mockUser.Setup(user => user.Email).Returns(Email);
+        mockUser.Setup(user => user.Address).Returns(NewAddress);
+        mockUser.Setup(user => user.PasswordHash).Returns(HashPassword(DifferentPassword));
+        
+        return mockUser.Object;
+    }
+    
     [TestMethod]
     public void CanCreateUserService_Ok()
     {
@@ -19,92 +45,72 @@ public class UserServiceTest
     [TestMethod]
     public void CanSignUpUser_Ok()
     {
-        var mockUser = new Mock<IUser>();
-        mockUser.Setup(user => user.Email).Returns("TestEmail@gmail.com");
+        var mockUser = GetMockUser();
         var mockRepository = new Mock<IUserRepository>();
         
         var userService = new UserService(mockRepository.Object);
-        userService.SignUp(mockUser.Object);
+        userService.SignUp(mockUser);
     }
     
     [ExpectedException(typeof(RepositoryException))]
     [TestMethod]
     public void CanSignUpUser_RepeatedUser_Throw()
     {
-        var mockUser = new Mock<IUser>();
-        mockUser.Setup(user => user.Email).Returns("TestEmail@gmail.com");
+        var mockUser = GetMockUser();
         
         var mockRepository = new Mock<IUserRepository>();
-        mockRepository.Setup(repo => repo.Get(mockUser.Object.Email)).Returns(mockUser.Object);
+        mockRepository.Setup(repo => repo.Get(mockUser.Email)).Returns(mockUser);
         
         var userService = new UserService(mockRepository.Object);
-        userService.SignUp(mockUser.Object);
+        userService.SignUp(mockUser);
     }
     
     [ExpectedException(typeof(RepositoryException))]
     [TestMethod]
     public void CanLogInUser_UnregisteredUser_Throw()
     {
-        const string email = "TestEmail@gmail.com";
-        const string password = "currentPassword";
-
-        var mockUser = new Mock<IUser>();
-        mockUser.Setup(user => user.Email).Returns(email);
+        var mockUser = GetMockUser();
 
         var mockRepository = new Mock<IUserRepository>();
-        mockRepository.Setup(repo => repo.Get(mockUser.Object.Email)).Returns(() => null);
+        mockRepository.Setup(repo => repo.Get(mockUser.Email)).Returns(() => null);
         
         var userService = new UserService(mockRepository.Object);
-        userService.LogIn(email, password);
+        userService.LogIn(Email, Password);
     }
     
     [ExpectedException(typeof(RepositoryException))]
     [TestMethod]
     public void CanLogInUser_WrongPassword_Throw()
     {
-        const string email = "TestEmail@gmail.com";
-        const string password = "differentPassword";
-        
-        var mockUser = new Mock<IUser>();
-        mockUser.Setup(user => user.Email).Returns(email);
-        mockUser.Setup(user => user.PasswordHash).Returns(HashPassword("currentPassword"));
-
-        var mockToCheckUser = new Mock<IUser>();
-        mockToCheckUser.Setup(user => user.Email).Returns(email);
-        mockToCheckUser.Setup(user => user.PasswordHash).Returns(HashPassword(password));
+        var mockUser = GetMockUser();
         
         var mockRepository = new Mock<IUserRepository>();
-        mockRepository.Setup(repo => repo.Get(email)).Returns(mockUser.Object);
+        mockRepository.Setup(repo => repo.Get(Email)).Returns(mockUser);
 
         var userService = new UserService(mockRepository.Object);
-        userService.LogIn(email, password);
+        userService.LogIn(Email, DifferentPassword);
     }
     
     [TestMethod]
     public void CanLogInUser_Ok()
     {
-        const string email = "TestEmail@gmail.com";
-        const string password = "currentPassword";
-
-        var mockUser = new Mock<IUser>();
-        mockUser.Setup(user => user.Email).Returns(email);
-        mockUser.Setup(user => user.PasswordHash).Returns(HashPassword(password));
+        var mockUser = GetMockUser();
 
         var mockRepository = new Mock<IUserRepository>();
-        mockRepository.Setup(repo => repo.Get(email)).Returns(mockUser.Object);
+        mockRepository.Setup(repo => repo.Get(Email)).Returns(mockUser);
 
         var userService = new UserService(mockRepository.Object);
-        var loggedInUser = userService.LogIn(email, password);
+        var loggedInUser = userService.LogIn(Email, Password);
         
-        Assert.AreEqual(mockUser.Object, loggedInUser);
+        Assert.AreEqual(mockUser, loggedInUser);
     }
 
-    private string HashPassword(string password)
+    private string HashPassword(string Password)
     {
         const int saltFactor = 12;
             
         var salt = BCrypt.Net.BCrypt.GenerateSalt(saltFactor);
-        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(Password, salt);
 
         return hashedPassword;
     }
@@ -113,58 +119,39 @@ public class UserServiceTest
     [TestMethod]
     public void CanDeleteUser_AlreadyDeletedUser_Throw()
     {
-        const string email = "TestEmail@gmail.com";
-        const string password = "currentPassword";
-
-        var mockUser = new Mock<IUser>();
-        mockUser.Setup(user => user.Email).Returns(email);
-        mockUser.Setup(user => user.PasswordHash).Returns(HashPassword(password));
+        var mockUser = GetMockUser();
 
         var mockRepository = new Mock<IUserRepository>();
-        mockRepository.Setup(repo => repo.Get(email)).Returns(() => null);
+        mockRepository.Setup(repo => repo.Get(Email)).Returns(() => null);
 
         var userService = new UserService(mockRepository.Object);
-        userService.DeleteUser(mockUser.Object);
+        userService.DeleteUser(mockUser);
     }
     
     [TestMethod]
     public void CanDeleteUser_Ok()
     {
-        const string email = "TestEmail@gmail.com";
-        const string password = "currentPassword";
-
-        var mockUser = new Mock<IUser>();
-        mockUser.Setup(user => user.Email).Returns(email);
-        mockUser.Setup(user => user.PasswordHash).Returns(HashPassword(password));
+        var mockUser = GetMockUser();
 
         var mockRepository = new Mock<IUserRepository>();
-        mockRepository.Setup(repo => repo.Get(email)).Returns(mockUser.Object);
+        mockRepository.Setup(repo => repo.Get(Email)).Returns(mockUser);
 
         var userService = new UserService(mockRepository.Object);
-        userService.DeleteUser(mockUser.Object);
+        userService.DeleteUser(mockUser);
     }
     
     [ExpectedException(typeof(RepositoryException))]
     [TestMethod]
     public void CanUpdateUser_NotExistingUser_Throw()
     {
-        const string email = "TestEmail@gmail.com";
-        const string toUpdateAddress = "1234 Laughter Lane";
-        const string newAddress = "101 Prankster Place";
-
-        var toUpdateMockUser = new Mock<IUser>();
-        toUpdateMockUser.Setup(user => user.Email).Returns(email);
-        toUpdateMockUser.Setup(user => user.Address).Returns(toUpdateAddress);
-
-        var mockUser = new Mock<IUser>();
-        mockUser.Setup(user => user.Email).Returns(email);
-        mockUser.Setup(user => user.Address).Returns(newAddress);
-
+        var toUpdateMockUser = GetMockUser();
+        var mockUser = GetSecondaryMockUser();
+        
         var mockRepository = new Mock<IUserRepository>();
-        mockRepository.Setup(repo => repo.Get(email)).Returns(() => null);
-        mockRepository.Setup(repo => repo.Update(toUpdateMockUser.Object));
+        mockRepository.Setup(repo => repo.Get(Email)).Returns(() => null);
+        mockRepository.Setup(repo => repo.Update(toUpdateMockUser));
         
         var userService = new UserService(mockRepository.Object);
-        userService.UpdateUser(mockUser.Object);
+        userService.UpdateUser(mockUser);
     }
 }
