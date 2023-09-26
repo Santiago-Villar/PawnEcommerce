4b45a9300fe0e1cfc4e33b1e8eac98d32195afb5
MateoGiraz <mateogiraz27@gmail.com>
2023-09-26 15:01:19 -0300

    RED SignUp_Ok

--- Test/Controller/UserControllerTest.cs
+++ Test/Controller/UserControllerTest.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PawnEcommerce.Controllers;
+using PawnEcommerce.DTO;
 using Service.User;
 
 namespace Test.Controller;
@@ -13,5 +15,23 @@ public class UserControllerTest
         Mock<IUserService> userServiceMock = new Mock<IUserService>();
         var userController = new UserController(userServiceMock.Object);
         Assert.IsNotNull(userController);
+    }
+    
+    [TestMethod]
+    public void SignUp_Ok()
+    {
+        var userServiceMock = new Mock<IUserService>();
+        var userController = new UserController(userServiceMock.Object);
+        var newUser = new UserCreateModel()
+        {
+            Email = "testEmail@gmail.com",
+            Password = "secret",
+            Address = "Lauro Müller 1776"
+        };
+
+        var result = userController.SignUp(newUser) as OkResult;
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(200, result.StatusCode);
     }
 }
