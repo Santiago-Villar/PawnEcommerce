ee65906cad66d22d7ef8ce24e052d7e4f6174e56
PabloLoading <pabloduransc@gmail.com>
2023-09-25 22:00:57 -0300

    GREEN GetPromotion_Ok

--- Service/Promotion/PromotionLogic.cs
+++ Service/Promotion/PromotionLogic.cs
@@ -5,14 +5,22 @@ namespace Service.Promotion
 {
 	public class PromotionLogic : IPromotionLogic
 	{
+		private PromotionSelector Selector { get; set; }
+
 		public PromotionLogic()
 		{
-			
+			Selector = new PromotionSelector();
 		}
 
 		public IPromotionStrategy GetPromotion(List<IProduct> products)
 		{
-			throw new ServiceException("Can not get promotion of empty list of products");
+			if (products.Count == 0)
+			{
+				throw new ServiceException("Can not get promotion of empty list of products");
+			}
+
+			return Selector.GetBestPromotion(products);
+
 		}
 	}
 }
