8d43106976ef95a88e07270c470e701cee4f4a09
PabloLoading <pabloduransc@gmail.com>
2023-09-26 20:46:01 -0300

    RED CanCreateSessionService_Ok

---
+++ Test/Service/Session/SessionServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Moq;
+using Service.User;
+
+namespace Test.Service.Session
+{
+    [TestClass]
+    public class SessionServiceTest
+	{
+		public SessionServiceTest()
+		{
+		}
+
+        [TestMethod]
+        public void CanCreateUserService_Ok()
+        {
+            ISessionService userService = new SessionService();
+            Assert.IsNotNull(userService);
+        }
+    }
+}
+
