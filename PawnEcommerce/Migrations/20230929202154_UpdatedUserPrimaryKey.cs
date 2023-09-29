adb25f9fcbcda212b4e8e325570b9970be551e81
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-09-29 17:29:15 -0300

    Removed [Key] property from User's Email attribute to ensure proper functioning of EF Core

---
+++ PawnEcommerce/Migrations/20230929202154_UpdatedUserPrimaryKey.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PawnEcommerce.Migrations
+{
+    public partial class UpdatedUserPrimaryKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Users_UserEmail",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sales_UserEmail",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "UserEmail",
+                table: "Sales");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Sales",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PromotionName",
+                table: "Sales",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Sales",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Colors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_UserId",
+                table: "Sales",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Users_UserId",
+                table: "Sales",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Users_UserId",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sales_UserId",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "PromotionName",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Colors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmail",
+                table: "Sales",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Email");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_UserEmail",
+                table: "Sales",
+                column: "UserEmail");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Users_UserEmail",
+                table: "Sales",
+                column: "UserEmail",
+                principalTable: "Users",
+                principalColumn: "Email",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
