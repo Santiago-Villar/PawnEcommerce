badec7ff723a66b7fca6c5db18ee8274bb065f5b
PabloLoading <pabloduransc@gmail.com>
2023-09-30 15:30:40 -0300

    GREEN EmptyBrandControllerMethod

---
+++ Test/Controller/BrandControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Moq;
+using PawnEcommerce.Controllers;
+using Service.Session;
+
+namespace Test.Controller
+{
+	[TestClass]
+	public class BrandControllerTest
+	{
+		public BrandControllerTest()
+		{
+		}
+
+        [TestMethod]
+        public void EmptyBrandControllerMethod()
+        {
+        }
+    }
+}
+
