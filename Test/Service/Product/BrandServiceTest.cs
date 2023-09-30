3c579fa892ab11c25de5f7b908417b2c6e7074bb
PabloLoading <pabloduransc@gmail.com>
2023-09-30 13:07:50 -0300

    RED CanGetAllBrands_Ok

--- Test/Service/Product/BrandServiceTest.cs
+++ Test/Service/Product/BrandServiceTest.cs
@@ -16,6 +16,26 @@ namespace Test
             IBrandService service = new BrandService(brandRepository);
             Assert.IsNotNull(service);
         }
+
+        [TestMethod]
+        public void CanGetAllBrands_Ok()
+        {
+            var brandRepository = new Mock<IBrandRepository>();
+            var brandsList = new List<Brand>
+            {
+                new Brand() { Name = "Kova" },
+                new Brand() { Name = "Brand2" },
+                new Brand() { Name = "Brand3" }
+            };
+            brandRepository.Setup(repo => repo.GetAll()).Returns(brandsList);
+
+
+            IBrandService service = new BrandService(brandRepository.Object);
+            List<Brand> brands = service.GetAll();
+            Assert.AreEqual(brands.Count, 3);
+            CollectionAssert.Contains(brands, brandsList[0]);
+        }
+
     }
 }
 
