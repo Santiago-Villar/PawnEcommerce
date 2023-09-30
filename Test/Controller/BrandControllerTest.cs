6f4e03f7d885f8977ac5d36180ca8853a46b101c
PabloLoading <pabloduransc@gmail.com>
2023-09-30 15:50:11 -0300

    RED CanGetAllBrands_Ok

--- Test/Controller/BrandControllerTest.cs
+++ Test/Controller/BrandControllerTest.cs
@@ -19,6 +19,25 @@ namespace Test.Controller
             var brandController = new BrandController(userServiceMock.Object);
             Assert.IsNotNull(brandController);
         }
+
+        [TestMethod]
+        public void CanGetAllBrands_Ok()
+        {
+            Mock<IBrandService> userServiceMock = new Mock<IBrandService>();
+            var brandsList = new List<Brand>
+            {
+                new Brand(1) { Name = "Kova" },
+                new Brand(2) { Name = "Brand2" },
+                new Brand(3) { Name = "Brand3" }
+            };
+            userServiceMock.Setup(service => service.GetAll()).Returns(brandsList);
+
+            var brandController = new BrandController(userServiceMock.Object);
+            List<Brand> brands = brandController.GetAll();
+
+            Assert.AreEqual(brands.Count, 3);
+            CollectionAssert.Contains(brands, brandsList[0]);
+        }
     }
 }
 
