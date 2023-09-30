3b8d43934829bbe0bca50ec5236b45afc204bfd5
MateoGiraz <mateogiraz27@gmail.com>
2023-09-29 21:12:47 -0300

    RED GetAll_Ok

--- Test/Controller/ProductControllerTest.cs
+++ Test/Controller/ProductControllerTest.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PawnEcommerce.Controllers;
 using Service.Product;
@@ -14,5 +15,21 @@ public class ProductControllerTest
         var productSerivce = new Mock<IProductService>();
         var productController = new ProductController(productSerivce.Object);
         Assert.IsNotNull(productController);
+    }
+    
+    [TestMethod]
+    public void GetAll_Ok()
+    {
+        var product1 = new Product { Name = "product1" };
+        var products = Enumerable.Repeat(product1, 3).ToArray();
+       
+        var productService = new Mock<IProductService>();
+        productService.Setup(ps => ps.GetAllProducts()).Returns(products);
+       
+        var productController = new ProductController(productService.Object);
+        var result = productController.GetAll() as OkObjectResult;
+        
+        Assert.IsNotNull(result);
+        CollectionAssert.AreEqual(products, result.Value as Array);
     }
 }
