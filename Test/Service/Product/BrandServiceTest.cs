6e5e34f7100bed60c0dcf0fb25a1715df3cd708c
PabloLoading <pabloduransc@gmail.com>
2023-09-30 14:57:38 -0300

    RED CanGetBrandById_Ok

--- Test/Service/Product/BrandServiceTest.cs
+++ Test/Service/Product/BrandServiceTest.cs
@@ -36,6 +36,34 @@ namespace Test
             CollectionAssert.Contains(brands, brandsList[0]);
         }
 
+        [TestMethod]
+        public void CanGetBrandById_Ok()
+        {
+            var brandRepository = new Mock<IBrandRepository>();
+            var brandsList = new List<Brand>
+            {
+                new Brand(1) { Name = "Kova" },
+                new Brand(2) { Name = "Brand2" },
+                new Brand(3) { Name = "Brand3" }
+            };
+            brandRepository.Setup(repo => repo.GetById(It.IsAny<int>()))
+                   .Returns<int>(id => brandsList.FirstOrDefault(b => b.Id == id));
+
+
+            IBrandService service = new BrandService(brandRepository.Object);
+
+            Brand brand1 = service.Get(1);
+            Brand brand2 = service.Get(2);
+
+            Assert.AreEqual(brand1.Name, brandsList[0].Name);
+            Assert.AreEqual(brand1.Id, brandsList[0].Id);
+
+            Assert.AreEqual(brand2.Name, brandsList[1].Name);
+            Assert.AreEqual(brand2.Id, brandsList[1].Id);
+
+        }
+
+
     }
 }
 
