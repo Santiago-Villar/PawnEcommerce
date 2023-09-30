cfa78cff02ad207ad42177a8b6b2c61894824c1f
PabloLoading <pabloduransc@gmail.com>
2023-09-30 15:41:51 -0300

    GREEN CanCreateController_Ok

---
+++ PawnEcommerce/Controllers/BrandController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Service.Product;
+
+namespace PawnEcommerce.Controllers
+{
+	public class BrandController
+	{
+        private IBrandService _brandService { get; set; }
+
+        public BrandController(IBrandService service)
+		{
+            _brandService = service;
+        }
+	}
+}
+
