fbfaf6b6dc655f4c1632b004e1c5d9045fdb99d8
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-09-30 00:45:17 -0300

    RED - AddProduct_WithValidProduct_ShouldCallAddAndSaveChanges fails

---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using Service.Product;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+
+namespace Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly EcommerceContext _context;
+        private bool _disposed = false;
+
+        public ProductRepository(EcommerceContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public void AddProduct(Product newProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product[] GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductByName(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product newProductVersion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test/Repository/ProductRepositoryTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using global::Service.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Repository;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test
+{
+
+
+    [TestClass]
+    public class ProductRepositoryTests
+    {
+        private Mock<EcommerceContext> _mockContext;
+        private Mock<DbSet<Product>> _mockDbSet;
+        private ProductRepository _repository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product A" },
+            new Product { Id = 2, Name = "Product B" }
+        }.AsQueryable();
+
+            _mockDbSet = new Mock<DbSet<Product>>();
+            _mockDbSet.As<IQueryable<Product>>().Setup(m => m.Provider).Returns(products.Provider);
+            _mockDbSet.As<IQueryable<Product>>().Setup(m => m.Expression).Returns(products.Expression);
+            _mockDbSet.As<IQueryable<Product>>().Setup(m => m.ElementType).Returns(products.ElementType);
+            _mockDbSet.As<IQueryable<Product>>().Setup(m => m.GetEnumerator()).Returns(products.GetEnumerator());
+
+            _mockContext = new Mock<EcommerceContext>();
+            _mockContext.Setup(c => c.Products).Returns(_mockDbSet.Object);
+
+            _repository = new ProductRepository(_mockContext.Object);
+        }
+
+        [TestMethod]
+        public void AddProduct_WithValidProduct_ShouldCallAddAndSaveChanges()
+        {
+            var newProduct = new Product { Id = 3, Name = "Product C" };
+
+            _repository.AddProduct(newProduct);
+
+            _mockDbSet.Verify(m => m.Add(It.IsAny<Product>()), Times.Once());
+            _mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+
+
+
+    }
+}
