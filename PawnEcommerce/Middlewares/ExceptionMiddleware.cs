2d285ab5697af6b430fc0c2cfcf6a7a87f767a33
MateoGiraz <mateogiraz27@gmail.com>
2023-09-29 22:00:02 -0300

    GREEN OnException_RepositoryException_Returns404

---
+++ PawnEcommerce/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,24 @@
+using System.Security.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Service.Exception;
+
+namespace PawnEcommerce.Middlewares;
+
+public class ExceptionMiddleware
+{
+    public class ExceptionFilter : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            try
+            {
+                throw context.Exception;
+            }
+            catch (RepositoryException e)
+            {
+                context.Result = new ObjectResult(new { Message = e.Message }) { StatusCode = 404 };
+            }
+        }
+    }
+}
