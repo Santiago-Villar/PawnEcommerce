7081e9d309e856a6cc1fdf24e86838d32df63e44
PabloLoading <pabloduransc@gmail.com>
2023-09-30 23:20:50 -0300

    RED CanGetAllCategories_Ok

--- Test/Service/Product/CategoryServiceTest.cs
+++ Test/Service/Product/CategoryServiceTest.cs
@@ -18,6 +18,25 @@ namespace Test
             ICategoryService service = new CategoryService(categoryRepository);
             Assert.IsNotNull(service);
         }
+
+        [TestMethod]
+        public void CanGetAllCategories_Ok()
+        {
+            var categoryRepository = new Mock<ICategoryRepository>();
+            var categoriesList = new List<Category>
+            {
+                new Category() { Name = "Kova" },
+                new Category() { Name = "Brand2" },
+                new Category() { Name = "Brand3" }
+            };
+            categoryRepository.Setup(repo => repo.GetAll()).Returns(categoriesList);
+            ICategoryService service = new CategoryService(categoryRepository.Object);
+
+            List<Brand> categories = service.GetAll();
+
+            Assert.AreEqual(categories.Count, 3);
+            CollectionAssert.Contains(categories, categoriesList[0]);
+        }
     }
 }
 
