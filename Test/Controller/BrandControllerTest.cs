6b81d23712fc02087b4a69dc7ddfd326251f27a2
PabloLoading <pabloduransc@gmail.com>
2023-09-30 22:31:40 -0300

    RED CanGetBrandById_Ok

--- Test/Controller/BrandControllerTest.cs
+++ Test/Controller/BrandControllerTest.cs
@@ -38,6 +38,31 @@ namespace Test.Controller
             Assert.AreEqual(brands.Count, 3);
             CollectionAssert.Contains(brands, brandsList[0]);
         }
+
+        [TestMethod]
+        public void CanGetBrandById_Ok()
+        {
+            Mock<IBrandService> userServiceMock = new Mock<IBrandService>();
+            var brandsList = new List<Brand>
+            {
+                new Brand(1) { Name = "Kova" },
+                new Brand(2) { Name = "Brand2" },
+                new Brand(3) { Name = "Brand3" }
+            };
+            userServiceMock.Setup(service => service.GetById(It.IsAny<int>()))
+                   .Returns<int>(id => brandsList.FirstOrDefault(b => b.Id == id));
+
+            var brandController = new BrandController(userServiceMock.Object);
+
+            Brand brand1 = brandController.Get(1);
+            Brand brand2 = brandController.Get(2);
+
+            Assert.AreEqual(brand1.Name, brandsList[0].Name);
+            Assert.AreEqual(brand1.Id, brandsList[0].Id);
+
+            Assert.AreEqual(brand2.Name, brandsList[1].Name);
+            Assert.AreEqual(brand2.Id, brandsList[1].Id);
+        }
     }
 }
 
