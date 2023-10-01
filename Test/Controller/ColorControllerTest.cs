209281f2b63b86f5a8f7bdf7cf49a3540f2a7006
PabloLoading <pabloduransc@gmail.com>
2023-10-01 11:10:43 -0300

    RED CanGetAllColors_Ok

--- Test/Controller/ColorControllerTest.cs
+++ Test/Controller/ColorControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PawnEcommerce.Controllers;
 using Service.Product;
@@ -15,6 +16,28 @@ namespace Test.Controller
             var controller = new ColorController(serviceMock.Object);
             Assert.IsNotNull(controller);
         }
+
+        [TestMethod]
+        public void CanGetAllColors_Ok()
+        {
+            var serviceMock = new Mock<IColorService>();
+            var controller = new ColorController(serviceMock.Object);
+
+            var colorsList = new List<Color>
+            {
+                new Color(1) { Name = "light Red", Code = "#2ASD2D" },
+                new Color(2) { Name = "DARKY BLUEE", Code = "#AF1FFF"},
+                new Color(3) { Name = "Dark asf", Code = "000000" }
+            };
+            serviceMock.Setup(repo => repo.GetAll()).Returns(colorsList);
+
+            var result = controller.GetAll() as OkObjectResult;
+            var colors = result.Value as List<Color>;
+
+            Assert.AreEqual(colors.Count, 3);
+            CollectionAssert.Contains(colors, colorsList[0]);
+        }
     }
+
 }
 
