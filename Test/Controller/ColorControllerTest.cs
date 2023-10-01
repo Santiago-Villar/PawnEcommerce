c6c7e5917b8f9edab1388772f263e6a4098562cf
PabloLoading <pabloduransc@gmail.com>
2023-10-01 11:16:19 -0300

    REFACTOR

--- Test/Controller/ColorControllerTest.cs
+++ Test/Controller/ColorControllerTest.cs
@@ -9,33 +9,40 @@ namespace Test.Controller
     [TestClass]
     public class ColorControllerTest
 	{
-        [TestMethod]
-        public void CanCreateController_Ok()
-        {
-            var serviceMock = new Mock<IColorService>();
-            var controller = new ColorController(serviceMock.Object);
-            Assert.IsNotNull(controller);
-        }
+        private Mock<IColorService> _colorServiceMock;
+        private ColorController _colorController;
+        private List<Color> _colorsList;
 
-        [TestMethod]
-        public void CanGetAllColors_Ok()
+        [TestInitialize]
+        public void SetUp()
         {
-            var serviceMock = new Mock<IColorService>();
-            var controller = new ColorController(serviceMock.Object);
+            _colorServiceMock = new Mock<IColorService>();
+            _colorController = new ColorController(_colorServiceMock.Object);
 
-            var colorsList = new List<Color>
+            _colorsList = new List<Color>
             {
                 new Color(1) { Name = "light Red", Code = "#2ASD2D" },
                 new Color(2) { Name = "DARKY BLUEE", Code = "#AF1FFF"},
                 new Color(3) { Name = "Dark asf", Code = "000000" }
             };
-            serviceMock.Setup(repo => repo.GetAll()).Returns(colorsList);
+        }
 
-            var result = controller.GetAll() as OkObjectResult;
+        [TestMethod]
+        public void CanCreateController_Ok()
+        {
+            Assert.IsNotNull(_colorController);
+        }
+
+        [TestMethod]
+        public void CanGetAllColors_Ok()
+        {
+            _colorServiceMock.Setup(repo => repo.GetAll()).Returns(_colorsList);
+
+            var result = _colorController.GetAll() as OkObjectResult;
             var colors = result.Value as List<Color>;
 
             Assert.AreEqual(colors.Count, 3);
-            CollectionAssert.Contains(colors, colorsList[0]);
+            CollectionAssert.Contains(colors, _colorsList[0]);
         }
     }
 
