44349c5753a5ba97327d58b37cc5f71cc6784c41
PabloLoading <pabloduransc@gmail.com>
2023-10-01 01:08:30 -0300

    REFACTOR

--- Test/Controller/CategoryControllerTest.cs
+++ Test/Controller/CategoryControllerTest.cs
@@ -8,63 +8,60 @@ namespace Test.Controller
 {
     [TestClass]
     public class CategoryControllerTest
-	{
+    {
+        private Mock<ICategoryService> _categoryServiceMock;
+        private CategoryController _categoryController;
+        private List<Category> _categoriesList;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _categoryServiceMock = new Mock<ICategoryService>();
+            _categoryController = new CategoryController(_categoryServiceMock.Object);
+            _categoriesList = new List<Category>
+            {
+                new Category(1) { Name = "Kova" },
+                new Category(2) { Name = "Category2" },
+                new Category(3) { Name = "Category3" }
+            };
+        }
+
         [TestMethod]
         public void CanCreateController_Ok()
         {
-           var categoryServiceMock = new Mock<ICategoryService>();
-           var categoryController = new CategoryController(categoryServiceMock.Object);
-           Assert.IsNotNull(categoryController);
+            Assert.IsNotNull(_categoryController);
         }
 
         [TestMethod]
         public void CanGetAllBrands_Ok()
         {
-            var categoryServiceMock = new Mock<ICategoryService>();
-            var categoryController = new CategoryController(categoryServiceMock.Object);
-            var categoriesList = new List<Category>
-            {
-                new Category(1) { Name = "Kova" },
-                new Category(2) { Name = "Category2" },
-                new Category(3) { Name = "Category3" }
-            };
-            categoryServiceMock.Setup(service => service.GetAll()).Returns(categoriesList);
+            _categoryServiceMock.Setup(service => service.GetAll()).Returns(_categoriesList);
 
-            var result = categoryController.GetAll() as OkObjectResult;
+            var result = _categoryController.GetAll() as OkObjectResult;
             var categories = result.Value as List<Category>;
 
             Assert.IsNotNull(result);
             Assert.AreEqual(categories.Count, 3);
-            CollectionAssert.Contains(categories, categoriesList[0]);
+            CollectionAssert.Contains(categories, _categoriesList[0]);
         }
 
         [TestMethod]
         public void CanGetBrandById_Ok()
         {
-            var categoryServiceMock = new Mock<ICategoryService>();
-            var categoryController = new CategoryController(categoryServiceMock.Object);
-            var categoriesList = new List<Category>
-            {
-                new Category(1) { Name = "Kova" },
-                new Category(2) { Name = "Category2" },
-                new Category(3) { Name = "Category3" }
-            };
+            _categoryServiceMock.Setup(service => service.Get(It.IsAny<int>()))
+                   .Returns<int>(id => _categoriesList.FirstOrDefault(b => b.Id == id));
 
-            categoryServiceMock.Setup(service => service.Get(It.IsAny<int>()))
-                   .Returns<int>(id => categoriesList.FirstOrDefault(b => b.Id == id));
-
-            var result1 = categoryController.Get(1) as OkObjectResult;
+            var result1 = _categoryController.Get(1) as OkObjectResult;
             var category1 = result1.Value as Category;
 
-            var result2 = categoryController.Get(2) as OkObjectResult;
+            var result2 = _categoryController.Get(2) as OkObjectResult;
             var category2 = result2.Value as Category;
 
-            Assert.AreEqual(category1.Name, categoriesList[0].Name);
-            Assert.AreEqual(category1.Id, categoriesList[0].Id);
+            Assert.AreEqual(category1.Name, _categoriesList[0].Name);
+            Assert.AreEqual(category1.Id, _categoriesList[0].Id);
 
-            Assert.AreEqual(category2.Name, categoriesList[1].Name);
-            Assert.AreEqual(category2.Id, categoriesList[1].Id);
+            Assert.AreEqual(category2.Name, _categoriesList[1].Name);
+            Assert.AreEqual(category2.Id, _categoriesList[1].Id);
         }
     }
 }
-
