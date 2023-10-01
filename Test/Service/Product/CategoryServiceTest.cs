5b270036da4a5de7f2c9ca81f645dd0e44553b8d
PabloLoading <pabloduransc@gmail.com>
2023-10-01 00:24:10 -0300

    RED CanGetCategoryById_Ok

--- Test/Service/Product/CategoryServiceTest.cs
+++ Test/Service/Product/CategoryServiceTest.cs
@@ -37,6 +37,32 @@ namespace Test
             Assert.AreEqual(categories.Count, 3);
             CollectionAssert.Contains(categories, categoriesList[0]);
         }
+
+        [TestMethod]
+        public void CanGetCategoryById_Ok()
+        {
+            var categoryRepository = new Mock<ICategoryRepository>();
+            var categoriesList = new List<Category>
+            {
+                new Category(1) { Name = "Kova" },
+                new Category(2) { Name = "Brand2" },
+                new Category(3) { Name = "Brand3" }
+            };
+            categoryRepository.Setup(repo => repo.GetById(It.IsAny<int>()))
+                   .Returns<int>(id => brandsList.FirstOrDefault(b => b.Id == id));
+
+
+            ICategoryService service = new CategoryService(categoryRepository.Object);
+
+            Brand brand1 = service.Get(1);
+            Brand brand2 = service.Get(2);
+
+            Assert.AreEqual(brand1.Name, categoriesList[0].Name);
+            Assert.AreEqual(brand1.Id, categoriesList[0].Id);
+
+            Assert.AreEqual(brand2.Name, categoriesList[1].Name);
+            Assert.AreEqual(brand2.Id, categoriesList[1].Id);
+        }
     }
 }
 
