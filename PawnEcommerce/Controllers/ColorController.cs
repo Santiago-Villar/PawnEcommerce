451364c269e25710f00fed653d793ecaecf55d1c
PabloLoading <pabloduransc@gmail.com>
2023-10-01 11:01:06 -0300

    GREEN CanCreateController_Ok

---
+++ PawnEcommerce/Controllers/ColorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Service.Product;
+
+namespace PawnEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    public class ColorController : ControllerBase
+    {
+        private IColorService _colorService { get; set; }
+
+        public ColorController(IColorService service)
+        {
+            _colorService = service;
+        }
+    }
+}
+
