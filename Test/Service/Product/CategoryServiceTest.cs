c0d500abfa77cd5506e8dd1b8431d9acc496eeb3
PabloLoading <pabloduransc@gmail.com>
2023-09-30 23:10:41 -0300

    GREEN EmptyMethodCategoryService

---
+++ Test/Service/Product/CategoryServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Moq;
+using Service.Product;
+
+namespace Test
+{
+    [TestClass]
+    public class CategoryServiceTest
+	{
+		public CategoryServiceTest()
+		{
+		}
+
+        [TestMethod]
+        public void EmptyMethodCategoryService()
+        {
+        }
+    }
+}
+
