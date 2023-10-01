b445d7ef0a79b5df6a1f61bcb6f818507a062f95
PabloLoading <pabloduransc@gmail.com>
2023-10-01 01:38:36 -0300

    RED CanGetAllColors_Ok

--- Test/Service/Product/ColorServiceTest.cs
+++ Test/Service/Product/ColorServiceTest.cs
@@ -14,6 +14,25 @@ namespace Test
             IColorService service = new ColorService(colorRepository);
             Assert.IsNotNull(service);
         }
+
+        [TestMethod]
+        public void CanGetAllColors_Ok()
+        {
+            var colorsRepository = new Mock<IColorRepository>();
+            var colorsList = new List<Color>
+            {
+                new Color(1) { Name = "light Red", Code = "#2ASD2D" },
+                new Color(2) { Name = "DARKY BLUEE", Code = "#AF1FFF"},
+                new Color(3) { Name = "Dark asf", Code = "000000" }
+            };
+            colorsRepository.Setup(repo => repo.GetAll()).Returns(colorsList);
+
+
+            IColorService service = new ColorService(colorsRepository.Object);
+            List<Color> colors = service.GetAll();
+            Assert.AreEqual(colors.Count, 3);
+            CollectionAssert.Contains(colors, colorsList[0]);
+        }
     }
 }
 
