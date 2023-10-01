141d9f06f715d773858b9821258f7b66936a21a1
PabloLoading <pabloduransc@gmail.com>
2023-10-01 00:52:19 -0300

    GREEN CanCreateController_Ok

---
+++ PawnEcommerce/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Service.Product;
+
+namespace PawnEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private ICategoryService _categoryService { get; set; }
+
+        public CategoryController(ICategoryService service)
+        {
+            _categoryService = service;
+        }
+    }
+}
