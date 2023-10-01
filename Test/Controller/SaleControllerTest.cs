691ae5d0053e8000d2523df1af162bcf9d878fb0
MateoGiraz <mateogiraz27@gmail.com>
2023-09-30 22:20:34 -0300

    RED Create_Ok

--- Test/Controller/SaleControllerTest.cs
+++ Test/Controller/SaleControllerTest.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PawnEcommerce.Controllers;
+using PawnEcommerce.DTO.Product;
+using PawnEcommerce.DTO.Sale;
 using Service.Sale;
 
 namespace Test.Controller;
@@ -15,4 +18,36 @@ public class SaleControllerTest
         Assert.IsNotNull(saleController);
     }
     
+    [TestMethod]
+    public void Create_Ok()
+    {
+        var saleService = new Mock<ISaleService>();
+        var saleController = new SaleController(saleService.Object);
+        
+        var productDto = new ProductDTO()
+        {
+            Name = "testProd",
+            Description = "test description",
+            Price = 10,
+            BrandName = "none",
+            CategoryName = "none",
+            Colors = new[] { "blue", "red" }
+        };
+                
+        var newSale = new SaleDTO()
+        {
+            UserId = 1,
+            ProductDtos = new ProductDTO[]
+            {
+                productDto
+            },
+        };
+        
+        var result = saleController.Create(newSale) as OkResult;
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(200, result.StatusCode);
+    }
 }
+
+    
