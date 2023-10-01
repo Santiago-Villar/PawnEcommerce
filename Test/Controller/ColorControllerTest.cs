17bb630bb28a35937e0bdb07eb191e3b6b76f65e
PabloLoading <pabloduransc@gmail.com>
2023-10-01 10:51:39 -0300

    GREEN EmptyMethodColorController

---
+++ Test/Controller/ColorControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PawnEcommerce.Controllers;
+
+namespace Test.Controller
+{
+    [TestClass]
+    public class ColorControllerTest
+	{
+        [TestMethod]
+        public void EmptyMethodColorController()
+        {
+        }
+    }
+}
+
