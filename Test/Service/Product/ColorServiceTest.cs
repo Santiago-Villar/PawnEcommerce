6834dfc54208d836a09a8d2152a740346e739c6b
PabloLoading <pabloduransc@gmail.com>
2023-10-01 01:49:45 -0300

    RED CanGetBrandById_Ok

--- Test/Service/Product/ColorServiceTest.cs
+++ Test/Service/Product/ColorServiceTest.cs
@@ -33,6 +33,33 @@ namespace Test
             Assert.AreEqual(colors.Count, 3);
             CollectionAssert.Contains(colors, colorsList[0]);
         }
+
+        [TestMethod]
+        public void CanGetBrandById_Ok()
+        {
+            var colorsRepository = new Mock<IColorRepository>();
+            var colorsList = new List<Color>
+            {
+                new Color(1) { Name = "light Red", Code = "#2ASD2D" },
+                new Color(2) { Name = "DARKY BLUEE", Code = "#AF1FFF"},
+                new Color(3) { Name = "Dark asf", Code = "000000" }
+            };
+            colorsRepository.Setup(repo => repo.GetById(It.IsAny<int>()))
+                   .Returns<int>(id => brandsList.FirstOrDefault(b => b.Id == id));
+
+
+            IColorService service = new ColorService(colorsRepository.Object);
+
+            Brand color1 = service.Get(1);
+            Brand color2 = service.Get(2);
+
+            Assert.AreEqual(color1.Name, colorsList[0].Name);
+            Assert.AreEqual(color1.Id, colorsList[0].Id);
+
+            Assert.AreEqual(color2.Name, colorsList[1].Name);
+            Assert.AreEqual(color2.Id, colorsList[1].Id);
+
+        }
     }
 }
 
