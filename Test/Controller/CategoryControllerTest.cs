6f06ab3138c13563b52e02baf7c583f26cce6295
PabloLoading <pabloduransc@gmail.com>
2023-10-01 00:55:15 -0300

    RED CanGetAllBrands_Ok

--- Test/Controller/CategoryControllerTest.cs
+++ Test/Controller/CategoryControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PawnEcommerce.Controllers;
 using Service.Product;
@@ -14,6 +15,26 @@ namespace Test.Controller
            var categoryServiceMock = new Mock<ICategoryService>();
            var categoryController = new CategoryController(categoryServiceMock.Object);
            Assert.IsNotNull(categoryController);
+        }
+
+        [TestMethod]
+        public void CanGetAllBrands_Ok()
+        {
+            var categoryServiceMock = new Mock<ICategoryService>();
+            var categoryController = new CategoryController(categoryServiceMock.Object);
+            var categoriesList = new List<Category>
+            {
+                new Category(1) { Name = "Kova" },
+                new Category(2) { Name = "Category2" },
+                new Category(3) { Name = "Category3" }
+            };
+            categoryServiceMock.Setup(service => service.GetAll()).Returns(categoriesList);
+
+            var result = categoryController.GetAll() as OkObjectResult;
+            var category = result.Value as List<Brand>;
+
+            Assert.AreEqual(category.Count, 3);
+            CollectionAssert.Contains(category, categoriesList[0]);
         }
     }
 }
