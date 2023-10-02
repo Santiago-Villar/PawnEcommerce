56ec667e6b2be7cfe62e21f725f0af9280e93337
MateoGiraz <mateogiraz27@gmail.com>
2023-10-02 20:57:38 -0300

    RED GetAll_Ok

---
+++ Test/Repository/ColorRepositoryTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository;
+using Service.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Repository
+{
+    [TestClass]
+    public class ColorRepositoryTest
+    {
+        private EcommerceContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<EcommerceContext>()
+                .UseInMemoryDatabase(databaseName: System.Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new EcommerceContext(options);
+            context.Database.EnsureDeleted();
+            return context;
+        }
+
+        private Color CreateSampleColor(EcommerceContext context)
+        {
+            var color = new Color(1)
+            {
+                Name = "Sample Color"
+            };
+            context.Colors.Add(color);
+            context.SaveChanges();
+
+            return color;
+        }
+
+        private Color CreateSampleColor2(EcommerceContext context)
+        {
+            var color = new Color(2)
+            {
+                Name = "Sample Color 2"
+            };
+            context.Colors.Add(color);
+            context.SaveChanges();
+
+            return color;
+        }
+
+        [TestMethod]
+        public void GetAll_Ok()
+        {
+            using var context = GetInMemoryDbContext();
+            var repository = new ColorRepository(context);
+
+            var color = CreateSampleColor(context);
+            var color2 = CreateSampleColor2(context);
+
+            context.Colors.Add(color);
+            context.Colors.Add(color2);
+
+            context.SaveChanges();
+
+            var colors = repository.GetAll();
+            Assert.AreEqual(2, colors.Count);
+        }
+    }
+}
+
