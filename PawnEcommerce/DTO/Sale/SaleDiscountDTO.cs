5c94e2dbbb695ad8f0c2614c27a8119156b256c4
MateoGiraz <mateogiraz27@gmail.com>
2023-10-04 17:19:10 -0300

    Create saleDiscountDto

---
+++ PawnEcommerce/DTO/Sale/SaleDiscountDTO.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using PawnEcommerce.DTO.Product;
+using Service.Product;
+using Service.Sale;
+
+namespace PawnEcommerce.DTO.Sale;
+
+public class SaleDiscountDTO
+{
+    public double price { get; set; }
+
+}
