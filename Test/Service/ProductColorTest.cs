40c1645625b6b7fd42df9eed007b2583f6c2e78d
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-10-03 21:32:55 -0300

    GREEN - ProductColor_AssociatesCorrectly passes

--- Test/Service/ProductColorTest.cs
+++ Test/Service/ProductColorTest.cs
@@ -61,6 +61,32 @@ namespace Test
             Assert.AreEqual(productColor.Color.Name, color.Name);
         }
 
+        [TestMethod]
+        public void ProductColor_AssociatesCorrectly()
+        {
+            var product = new Product
+            {
+                Id = 1,
+                Name = "TestProduct",
+                Price = 100
+            };
 
+            var color = new Color(1)
+            {
+                Name = "Red"
+            };
+
+            var productColor = new ProductColor
+            {
+                ProductId = product.Id,
+                Product = product,
+                ColorId = color.Id,
+                Color = color
+            };
+
+            Assert.AreEqual(productColor.Product.Id, productColor.ProductId);
+            Assert.AreEqual(productColor.Color.Id, productColor.ColorId);
+        }
+    }
 }
 
