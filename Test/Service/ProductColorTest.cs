e446b81ae23b2d07ebdd676dcc4d21540a559878
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-10-03 21:28:20 -0300

    GREEN - ProductColor_HasProduct passes

--- Test/Service/ProductColorTest.cs
+++ Test/Service/ProductColorTest.cs
@@ -7,7 +7,32 @@ namespace Test
     [TestClass]
     public class ProductColorTest
     {
+        [TestMethod]
+        public void ProductColor_HasProduct()
+        {
+            var product = new Product
+            {
+                Id = 1,
+                Name = "TestProduct",
+                Price = 100
+            };
 
+            var color = new Color(1)
+            {
+                Name = "Red"
+            };
+
+            var productColor = new ProductColor
+            {
+                ProductId = product.Id,
+                Product = product,
+                ColorId = color.Id,
+                Color = color
+            };
+
+            Assert.AreEqual(productColor.Product.Id, product.Id);
+            Assert.AreEqual(productColor.Product.Name, product.Name);
+        }
 
         [TestMethod]
         public void ProductColor_HasColor()
