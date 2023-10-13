fd3ec312629c27d8b11b4248a0e74012ebc1e1fd
MateoGiraz <mateogiraz27@gmail.com>
2023-10-12 22:58:36 -0300

    create login form

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+  
 export class LoginComponent {
 
+  email: String = '';
+  password: String = '';
+
+  constructor() {}
+
+  login(): void{
+    console.log("login test")
+  }
 }
