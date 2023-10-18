6696fbe6032c9370d72a797b910a327514fe520c
MateoGiraz <mateogiraz27@gmail.com>
2023-10-18 16:28:02 -0300

    update test products

--- front/src/app/components/cart/shopping-bag/TEST_PRODUCTS.ts
+++ front/src/app/components/cart/shopping-bag/TEST_PRODUCTS.ts
@@ -1,52 +1,39 @@
-export const PRODUCTS = [
+import { Product } from "src/app/models/product.model";
+
+export const PRODUCTS: Product[] = [
   {
-    "name": "laptop",
-    "price": "1200",
-    "color": "silver"
+    id: "1",
+    name: "laptop",
+    description: "A powerful laptop for all your computing needs.",
+    price: 1200,
+    color: [
+      { id: "1", name: "silver", code: "#C0C0C0" },
+      { id: "2", name: "white", code: "#FFFFFF" }
+    ],
+    brand: { id: "1", name: "Example Brand" },
+    category: { id: "1", name: "Electronics" }
   },
   {
-    "name": "sneakers",
-    "price": "75",
-    "color": "white"
+    id: "2",
+    name: "sneakers",
+    description: "Comfortable sneakers for everyday wear.",
+    price: 75,
+    color: [
+      { id: "2", name: "white", code: "#FFFFFF" }
+    ],
+    brand: { id: "2", name: "Footwear Co." },
+    category: { id: "2", name: "Footwear" }
   },
   {
-    "name": "smartphone",
-    "price": "800",
-    "color": "black"
+    id: "3",
+    name: "smartphone",
+    description: "A feature-packed smartphone with advanced technology.",
+    price: 800,
+    color: [
+      { id: "3", name: "black", code: "#000000" }
+    ],
+    brand: { id: "1", name: "Example Brand" },
+    category: { id: "1", name: "Electronics" }
   },
-  {
-    "name": "dress",
-    "price": "50",
-    "color": "blue"
-  },
-  {
-    "name": "coffee maker",
-    "price": "100",
-    "color": "red"
-  },
-  {
-    "name": "watch",
-    "price": "250",
-    "color": "gold"
-  },
-  {
-    "name": "backpack",
-    "price": "40",
-    "color": "gray"
-  },
-  {
-    "name": "blender",
-    "price": "60",
-    "color": "green"
-  },
-  {
-    "name": "sunglasses",
-    "price": "30",
-    "color": "brown"
-  },
-  {
-    "name": "headphones",
-    "price": "150",
-    "color": "black and silver"
-  }
-]
+];
+
