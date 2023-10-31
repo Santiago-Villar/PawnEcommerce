59b8ccdcfb41bf86b72b2dc5a7cc6dad5b8d4dd3
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-10-31 01:45:44 -0300

    RED - VerifyCartVerifyAndUpdateCart_SufficientStock_UpdatesCartWithLatestProductDetails fails

---
+++ back/Test/Utils/CartUtilsTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Service.Product;
+using System.Linq;
+using System.Collections.Generic;
+using Service.Filter.ConcreteFilter;
+
+namespace Test.Utilities
+{
+    [TestClass]
+    public class CartUtilsTest
+    {
+        private Mock<IProductService> _mockProductService;
+        private CartUtils _cartUtils;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockProductService = new Mock<IProductService>();
+            _cartUtils = new CartUtils(_mockProductService.Object);
+        }
+
+        [TestMethod]
+        public void VerifyAndUpdateCart_ProductNoLongerAvailable_RemovesFromCart()
+        {
+            var cartProduct = new Product { Id = 1 };
+            _mockProductService.Setup(s => s.GetAllProducts(It.IsAny<FilterQuery>())).Returns(new Product[] { });
+
+            var result = CartUtils.VerifyAndUpdateCart(new[] { cartProduct });
+
+            Assert.IsFalse(result.Any());  // Cart should be empty.
+        }
+
+        [TestMethod]
+        public void VerifyAndUpdateCart_SufficientStock_UpdatesCartWithLatestProductDetails()
+        {
+            var cartProduct = new Product { Id = 1 };
+            var latestProduct = new Product { Id = 1 }; // mock latest product details from the database
+            latestProduct.Stock=5; // Assume this method sets the stock for the product
+
+            _mockProductService.Setup(s => s.GetAllProducts(It.IsAny<FilterQuery>())).Returns(new[] { latestProduct });
+
+            var result = CartUtils.VerifyAndUpdateCart(new[] { cartProduct });
+
+            Assert.AreEqual(1, result.Length);
+            Assert.AreSame(latestProduct, result[0]);  // Make sure it's the latest product version
+        }
+
+        [TestMethod]
+        public void VerifyAndUpdateCart_InsufficientStock_RemovesProductFromCart()
+        {
+            var cartProduct = new Product { Id = 1 };
+            var latestProduct = new Product { Id = 1 };
+            latestProduct.Stock=0;  // No stock available
+
+            _mockProductService.Setup(s => s.GetAllProducts(It.IsAny<FilterQuery>())).Returns(new[] { latestProduct });
+
+            var result = CartUtils.VerifyAndUpdateCart(new[] { cartProduct });
+
+            Assert.IsFalse(result.Any());  // Product should be removed from the cart
+        }
+    }
+}
+
