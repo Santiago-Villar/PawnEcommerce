9a6754a852a83c1fb2b017f62da41feeeb29152e
Santiago-Villar <91506401+Santiago-Villar@users.noreply.github.com>
2023-10-31 11:08:28 -0300

    RED - CanCreateIdsFilter_Ok fails

---
+++ back/Test/Service/Filter/IdsFilterTest.cs
@@ -0,0 +1,16 @@
+﻿using Service.Exception;
+using Service.Filter;
+using Service.Filter.ConcreteFilter;
+using Service.Product;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Test.Service.Filter;
+
+[TestClass]
+[ExcludeFromCodeCoverage]
+public class IdsFilterTest
+{
+    [TestMethod]
+
+}
